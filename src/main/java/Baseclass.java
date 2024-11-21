09f7dd976466127e6a606cddf57d54bb3da82a3a
ravikanth.anisetty30@gmail.com <Ravikanth.Anisetty@ivycomptech.com>
2024-11-21 13:10:14 +0530

    1st commit-Nov24

---
+++ src/main/java/Baseclass.java
@@ -0,0 +1,22 @@
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Baseclass {
+
+	
+	int a=10;
+	int b=20;
+	
+ @Test
+public void test()
+{
+	int c=a+b;
+
+      Assert.assertEquals(true, true);
+
+	}
+	
+}
